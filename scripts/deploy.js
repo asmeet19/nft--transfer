80296887dacd44ce81a53de51f1872c48f28786f
asmeet19 <161880322+asmeet19@users.noreply.github.com>
2024-03-07 16:18:03 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -25,13 +25,3 @@ main().catch((error) => {
   process.exitCode = 1;
 });
 
-/*
-
-npm i
-npm install dotenv --save
-npx hardhat compile
-npx hardhat run scripts/deploy.js --network goerli
-node scripts/mint.js
-node .\scripts\approveDeposit.js
-
-*/
